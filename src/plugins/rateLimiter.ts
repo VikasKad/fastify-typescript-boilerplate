d036d2ce0f9f9e700a413c9b1daaca49c910cc1d
Vikas <vikasrkad@gmail.com>
2024-07-25 21:40:04 +0530

    Implement Swagger documentation plugin

--- src/plugins/rateLimiter.ts
+++ src/plugins/rateLimiter.ts
@@ -1,7 +1,8 @@
 import fp from 'fastify-plugin';
 import rateLimit from '@fastify/rate-limit';
+import { FastifyInstance } from 'fastify';
 
-async function rateLimiterPlugin(fastify, opts) {
+async function rateLimiterPlugin(fastify: FastifyInstance, opts: any) {
     fastify.register(rateLimit, {
         max: 100,
         timeWindow: '1 minute',
